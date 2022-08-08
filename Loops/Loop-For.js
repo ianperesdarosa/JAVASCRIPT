86cdb04c7fd843aeda89ff95eb17f68514a6ea03
ianperesdarosa <ianperesdarosa2004@gmail.com>
2022-08-08 20:51:47 -0300

    :fire JS

---
+++ Loops/Loop-For.js
@@ -0,0 +1,17 @@
+for(let x = 0; x < 10; x++){
+
+    Text += "The number is " + x + "<br>";
+}
+
+//Revisão
+
+for(let i = 2, len = cars.lenght; i < len; i++){
+    Text += cars[i] + "<br>";
+}
+
+
+//Teste 
+
+for(let num = 0; num < 10; num++){
+    Text += "The number is " + num + "<br>";
+}
