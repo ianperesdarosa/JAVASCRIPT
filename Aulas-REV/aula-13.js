c3ca8cb3575d17551f0b7b9d6c7e2793c7b78c31
ianperesdarosa <ianperesdarosa2004@gmail.com>
2022-09-16 21:49:45 -0300

    :fire:

---
+++ Aulas-REV/aula-13.js
@@ -0,0 +1,23 @@
+//Método shift():
+const cars = ["BMW", "Subaru", "Toyota", "Nissan"];
+cars.shift();
+
+//Método unshift();
+const car = ["BMW", "Subaru", "Toyota", "Nissan"];
+cars.unshift("Ford");
+
+//Método Delete:
+const aluno = ["João", "Ana", "Gabi", "Jonathan"];
+delete(aluno);
+
+//Métod Splice();
+const x = ["BMW", "Subaru", "Toyota", "Nissan"];
+x.splice(3, 0, "Ford");
+
+//Método slice();
+const z = ["João", "Ana", "Gabi", "Jonathan"];
+z.slice(3, 0);
+
+//Método Reverse();
+const carx = ["BMW", "Subaru", "Toyota", "Nissan"];
+carx.reverse();
