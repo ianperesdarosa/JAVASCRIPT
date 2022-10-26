f7a3fb8e158dcaf4f5e2577b6d16f689c3c8006d
ianperesdarosa <ianperesdarosa2004@gmail.com>
2022-10-26 20:37:06 -0300

    :fire:

--- Aulas/aula-16.js
+++ Aulas/aula-16.js
@@ -8,8 +8,59 @@ For (let i = 0; i < 5; i++) {
 */
 
 //pratica
-const cars = ["BMW", "Subaru", "Toyota", "Nissan"];
-
-for (let i = 0; i < cars.length; i++) {
-    text += cars[i] + "<br>";
+for (let i = 0; i < 5; i++) {
+    console.log("Olá, mundo!");
 }
+
+//Loops For in: iterra sobre elementos de uma Array ou Objeto
+
+//Sintaxe
+/*
+for (key in object) {
+
+    text += perosn[x];
+
+}
+*/
+
+//pratica
+const pessoa = {
+    nome: "ian",
+    idade: 18
+}
+
+for (let key in pessoa){
+    console.log(key, pessoa.nome,);
+}
+
+const cores = ["Red", "Blue", "Green", "Black"];
+
+for (let indi in cores) {
+    console.log(indi, cores[indi]);
+}
+
+//For of: muito mais facil de fazer iteraçoes "Unica difereça entre o de cima e que nao declara o indice da array/objeto"
+
+for (let cor of cores) {
+    console.log(cor)
+}
+
+
+//Loop while
+let i = 5;
+
+while (i >= 1) {
+    if (i % 2 !== 0){
+        console.log(i);
+    }
+    i--;
+}
+
+//Loop Do while "Quase nunca usa essa bosta!!!"
+
+let x = 0;
+
+do{
+    console.log("Digiando");
+    x++;
+} while (x < 10) 
