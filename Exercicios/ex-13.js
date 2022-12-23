28d4fd022ba822b100509f57d9c4482f2fb74d36
ianperesdarosa <ianperesdarosa2004@gmail.com>
2022-12-23 02:53:00 -0300

    :fire:

--- Exercicios/ex-13.js
+++ Exercicios/ex-13.js
@@ -5,3 +5,9 @@ for (let i = 0; i < 11; i++) {
 }
 
 //Me retorne uma constegem de numeros em ordem cresente do 10 ao 0
+ 
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+array.reverse();
+for (j of array) {
+    console.log(j)
+}
