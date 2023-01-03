7b1512361fed829e925230733b0bc45a9e2da23a
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-01-03 17:06:02 -0300

    :fire:

--- Aulas/aula-17.js
+++ Aulas/aula-17.js
@@ -1,5 +1,4 @@
 //Classes no Javascript
-
 class person{
     constructor (name) {
         this._name = name;
@@ -13,8 +12,30 @@ class person{
 let dados = new person("Ian Peres Da Rosa")
 console.log(dados.complete())
 
-//Set / Get
 
+//Set / Get
+class carro {
+    constructor (car){
+        this._car = car
+    }
+
+    get car () {
+        return this._car  
+    }
+
+    set model (model) {
+        this._model = model
+    }
+
+    set year (year) {
+        this._year = year
+    }
+}
+
+let carName = new carro ("BMW")
+carro.model = "E36"
+carro.year = 1996
+console.log(carName.car, carro.model, carro.year)
 
 
 //Herança
