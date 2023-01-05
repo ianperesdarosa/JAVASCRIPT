239e46a5128f59553badb5a0e1adfd9b06c858d3
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-01-05 01:07:20 -0300

    :fire:

--- Aulas-REV/aula-17.js
+++ Aulas-REV/aula-17.js
@@ -9,7 +9,7 @@ class estado {
     }
 }
 
-let dados = new estado ("Morto")
+let dados = new estado ("Vivo")
 console.log(dados.display())
 
 
@@ -38,4 +38,17 @@ identificacao.data = 2004
 console.log(card.name, identificacao.age, identificacao.data)
 
 //Herança
+class myCard extends identificacao {
+    constructor (cidadao, data, age) {
+        super(cidadao)
+        this._data = data
+        this._age = age
+    }
 
+    display () {
+        return `Nome: ${this._cidadao} Nascimento: ${this._data} Idade:${this._age}`
+    }
+}
+
+let inf = new myCard ("Ian", 2004, 18)
+console.log(inf.display())
